40196ec67d1831e0d5103d52ba205d05c85671e7
Iarlley <iarlleymangueira@gmail.com>
2025-04-24 13:54:31 -0300

    adicionado esqueleto do projeto

---
+++ src/main/java/com/engcode/bffagendadortarefas/BffAgendadorTarefasApplication.java
@@ -0,0 +1,15 @@
+package com.engcode.bffagendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BffAgendadorTarefasApplication.class, args);
+
+	}
+
+}
