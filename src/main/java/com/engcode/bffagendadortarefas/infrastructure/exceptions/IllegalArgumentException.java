fdb4cd86db85cb0dc465efac69277e211b89d489
Iarlley <iarlleymangueira@gmail.com>
2025-05-07 12:07:34 -0300

    Adicionado Gobal exception

---
+++ src/main/java/com/engcode/bffagendadortarefas/infrastructure/exceptions/IllegalArgumentException.java
@@ -0,0 +1,15 @@
+package com.engcode.bffagendadortarefas.infrastructure.exceptions;
+
+public class IllegalArgumentExcepition extends RuntimeException {
+
+    public IllegalArgumentExcepition(String menssagem) {
+
+        super(menssagem);
+    }
+
+    public IllegalArgumentExcepition(String menssagem, Throwable throwable) {
+
+        super(menssagem, throwable);
+    }
+
+}
